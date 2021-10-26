f921479c209e68071df58b1c367b5e29f39d12fc
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-26 17:05:42 +0200

    Added Two Fighter Card components into the Fight component

--- src/components/DisplayFight.jsx
+++ src/components/DisplayFight.jsx
@@ -1,7 +1,16 @@
 import './DisplayFight.css';
-
 import React from 'react';
 
+import FighterCard from './FighterCard';
+
 export default function DisplayFight() {
-  return <h2>FIGHT!</h2>;
+  return (
+    <>
+      <h2 className="fight-title">FIGHT!</h2>
+      <div className="heroes-display">
+        <FighterCard />
+        <FighterCard />
+      </div>
+    </>
+  );
 }
