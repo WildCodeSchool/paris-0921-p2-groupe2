a676cc91f6cb036bf16cc2ee6bc2623394f00a9a
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-27 15:48:09 +0200

    Added Displayfight (and example props) to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import './App.css';
 import DisplayFight from './components/DisplayFight';
 
 function App() {
+  const heroA = '18';
+  const heroB = '236';
+
   return (
     <>
-      <DisplayFight />
+      <DisplayFight heroA={heroA} heroB={heroB} />
     </>
   );
 }
