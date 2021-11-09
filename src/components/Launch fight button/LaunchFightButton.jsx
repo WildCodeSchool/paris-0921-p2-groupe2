2f180861c586002ef21503c97248d0f91d654ffc
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-11-09 15:07:46 +0100

    Added a reset button and usecontext functionality

--- src/components/Launch fight button/LaunchFightButton.jsx
+++ src/components/Launch fight button/LaunchFightButton.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import FighterContext from '../../contexts/FighterContext';
 
 import styles from './LaunchFightButton.module.css';
 
 export default function LaunchFightButton() {
+  const { resetPlayers } = useContext(FighterContext);
+
   return (
-    <>
+    <div className={styles.buttonsContainer}>
       <Link to="/fight">
         <button className={styles.launchFightButton}>FIGHT</button>
       </Link>
-    </>
+      <button className={styles.launchFightButton} onClick={resetPlayers}>
+        RESET
+      </button>
+    </div>
   );
 }
