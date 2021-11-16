42374365d2332a38fd4cc67a72b4b8c2cb412899
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-11-16 16:45:18 +0100

    Minimal pages to test route and back-end algo

--- src/Screens/Team mode Choice/TeamChoice.jsx
+++ src/Screens/Team mode Choice/TeamChoice.jsx
@@ -1,16 +1,31 @@
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import CharacterDetail from '../../components/Character detail/CharacterDetail';
-import CharactersList from '../../components/Characters list/CharactersList';
+import FighterContext from '../../contexts/FighterContext';
 
 import styles from './TeamChoice.module.css';
 
 export default function TeamChoice() {
-  const [selectedCharacter, setSelectedCharacter] = useState('');
-  const [heroesList, setHeroesList] = useState('');
-  const [query, setQuery] = useState('');
+  const { setTeamA, setTeamB } = useContext(FighterContext);
+  let defaultA = [70, 106, 107];
+  let defaultB = [127, 228, 236];
+  let tempA = [];
+  let tempB = [];
+
+  useEffect(() => {
+    for (let index of defaultA) {
+      fetch(`https://cors-bypass.tkzprod.dev/superheroapi.com/api/408055134055673/${index}`)
+        .then((res) => res.json())
+        .then((data) => tempA.push(data));
+    }
+    setTeamA(tempA);
+    for (let index of defaultB) {
+      fetch(`https://cors-bypass.tkzprod.dev/superheroapi.com/api/408055134055673/${index}`)
+        .then((res) => res.json())
+        .then((data) => tempB.push(data));
+    }
+    setTeamB(tempB);
+  }, []);
 
   return (
     <div className={styles.container}>
--- src/Screens/Team mode Fight/TeamFight.jsx
+++ src/Screens/Team mode Fight/TeamFight.jsx
@@ -1,29 +1,45 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { handicaps, weapons, fields } from '../../algorithms/bonusmalus/BonusMalus';
-import CombatAlgorithm from '../../algorithms/CombatAlgorithm';
-import Fighter from '../../algorithms/CharacterClass';
+import generateTeams from '../../algorithms/GenerateTeams';
+import teamAlgorithm from '../../algorithms/TeamAlgorithm';
 
 import FighterContext from '../../contexts/FighterContext';
-import FighterCard from '../../components/Fighter card/FighterCard';
-import FigthtingReport from '../../components/Figthing Report/FightingReport';
 
 import styles from './TeamFight.module.css';
 
 export default function TeamFight() {
-  const { playerA, playerB, resetGame, options } = useContext(FighterContext);
+  const { resetGame, teamA, teamB } = useContext(FighterContext);
+  let groupA, groupB;
+  let report = [];
 
-  //   const FighterA = new Fighter(playerA);
-  //   const FighterB = new Fighter(playerB);
+  useEffect(() => {
+    // console.log(teamA);
+    // console.log(teamB);
+    let groups = generateTeams(teamA, teamB);
+    groupA = groups[0];
+    groupB = groups[1];
+  }, []);
 
-  // let report = CombatAlgorithm(FighterA, FighterB);
+  //   useEffect(() => {
+  //     console.log(groupA);
+  //   }, [groupA]);
+
+  useEffect(() => {
+    if (groupA.length == 3 && groupB.length == 3) {
+      report = teamAlgorithm(groupA, groupB);
+    }
+  }, [groupA, groupB]);
+
+  // useEffect(() => {
+  //   if (report.length) {
+  //     console.log(report);
+  //   }
+  // }, [report]);
 
   return (
     <div className={styles.fightingComponent}>
-      {/* {report && <FigthtingReport report={report} />} */}
-
       <div className={styles.endgameChoice}>
         <Link to="/team">
           <button onClick={resetGame}>Another battle</button>
