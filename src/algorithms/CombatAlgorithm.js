de6c2d8564a0b07c502ad6f517043b1583dc1ecd
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-11-16 12:24:34 +0100

    Updated the algorithm to display pv percentages after each round

--- src/algorithms/CombatAlgorithm.js
+++ src/algorithms/CombatAlgorithm.js
@@ -33,27 +33,27 @@ export default function fight(fighterA, fighterB) {
       if (damage <= -100) {
         initiativeA = -100;
         lifeA -= 3;
-        rapport.push(`EPIC FAIL : ${fighterA.name} trips (-3PV => ${lifeA}/${initialLifeA}).`);
+        rapport.push([`EPIC FAIL : ${fighterA.name} trips.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else if (damage < -50) {
         initiativeA /= 2;
-        rapport.push(`${fighterB.name} dodges.`);
+        rapport.push([`${fighterB.name} dodges.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else if (damage < 0) {
         let punch = Math.floor(damageA * 0.1);
         lifeB -= punch;
-        rapport.push(`${first ? fighterA.name : pronoun(fighterA)} is blocked (-${punch}PV => ${lifeB}/${initialLifeB}).`);
+        rapport.push([`${first ? fighterA.name : pronoun(fighterA)} is blocked.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else if (damage < 50) {
         let punch = Math.floor(damageA * 0.2);
         lifeB -= punch;
-        rapport.push(`${first ? fighterA.name : pronoun(fighterA)} hits (-${punch}PV => ${lifeB}/${initialLifeB}).`);
+        rapport.push([`${first ? fighterA.name : pronoun(fighterA)} hits.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else if (damage < 100) {
         let punch = Math.floor(damageA * 0.3);
         lifeB -= punch;
-        rapport.push(`${first ? fighterA.name : pronoun(fighterA)} strikes ! (-${punch}PV => ${lifeB}/${initialLifeB}).`);
+        rapport.push([`${first ? fighterA.name : pronoun(fighterA)} strikes !`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else {
         initiativeA = fighterA.speed - 1;
         let punch = Math.floor(damageA * 0.5);
         lifeB -= punch;
-        rapport.push(`CRITICAL : ${fighterB.name} stumbles (-${punch}PV => ${lifeB}/${initialLifeB}).`);
+        rapport.push([`CRITICAL : ${fighterB.name} stumbles.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       }
     } else if (initiative == 'B') {
       first = initiativeB == fighterB.speed;
@@ -66,36 +66,34 @@ export default function fight(fighterA, fighterB) {
       if (damage <= -100) {
         initiativeB = -100;
         lifeB -= 3;
-        rapport.push(`EPIC FAIL : ${fighterB.name} trips (-3PV => ${lifeB}/${initialLifeB}).`);
+        rapport.push([`EPIC FAIL : ${fighterB.name} trips.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else if (damage < -33) {
         initiativeB /= 2;
-        rapport.push(`${fighterA.name} dodges.`);
+        rapport.push([`${fighterA.name} dodges.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else if (damage < 0) {
         let punch = Math.floor(damageB * 0.1);
         lifeA -= punch;
-        rapport.push(`${first ? fighterB.name : pronoun(fighterB)} is blocked (-${punch}PV => ${lifeA}/${initialLifeA}).`);
+        rapport.push([`${first ? fighterB.name : pronoun(fighterB)} is blocked.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else if (damage < 33) {
         let punch = Math.floor(damageB * 0.2);
         lifeA -= punch;
-        rapport.push(`${first ? fighterB.name : pronoun(fighterB)} hits (-${punch}PV => ${lifeA}/${initialLifeA}).`);
+        rapport.push([`${first ? fighterB.name : pronoun(fighterB)} hits.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else if (damage < 100) {
         let punch = Math.floor(damageB * 0.3);
         lifeA -= punch;
-        rapport.push(`${first ? fighterB.name : pronoun(fighterB)} strikes ! (-${punch}PV => ${lifeA}/${initialLifeA}).`);
+        rapport.push([`${first ? fighterB.name : pronoun(fighterB)} strikes !`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       } else {
         initiativeB = fighterB.speed - 1;
         let punch = Math.floor(damageB * 0.5);
         lifeA -= punch;
-        rapport.push(`CRITICAL : ${fighterA.name} stumbles (-${punch}PV => ${lifeA}/${initialLifeA}).`);
+        rapport.push([`CRITICAL : ${fighterA.name} stumbles.`, (lifeA / initialLifeA) * 100, (lifeB / initialLifeB) * 100]);
       }
     }
   }
   if (lifeA <= 0) {
-    rapport.push(`${fighterA.name} falls on the ground...`);
     rapport.push(`${fighterB.name} wins !`);
     return rapport;
   } else {
-    rapport.push(`${fighterB.name} falls on the ground...`);
     rapport.push(`${fighterA.name} wins !`);
     return rapport;
   }
