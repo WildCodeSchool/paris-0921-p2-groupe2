07e24bec62735b5efbfe1d18dd6262717a8338d8
isamj27950 <isamj27yahoo.fr>
2021-11-17 14:39:54 +0100

    fix about css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
 
 function Footer() {
   return (
     <div className={styles.footer}>
-      <p className={styles.hfooter}>About</p>
+      <Link to="/about">
+        <p className={styles.hfooter}>About</p>
+      </Link>
     </div>
   );
 }
