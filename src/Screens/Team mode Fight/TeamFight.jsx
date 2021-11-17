1119819e5e7b02b190337e1eb6d857276c68cd46
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-11-17 11:15:52 +0100

    Styled the team fight page

--- src/Screens/Team mode Fight/TeamFight.jsx
+++ src/Screens/Team mode Fight/TeamFight.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import generateTeams from '../../algorithms/GenerateTeams';
 import teamAlgorithm from '../../algorithms/TeamAlgorithm';
 
+import TeamCard from '../../components/Team card/TeamCard';
 import FighterContext from '../../contexts/FighterContext';
 import TeamFightReport from '../../components/Team Fight Report/TeamFightReport';
 
@@ -13,18 +14,17 @@ import styles from './TeamFight.module.css';
 export default function TeamFight() {
   const { resetGame, teamA, teamB } = useContext(FighterContext);
 
-  // console.log(teamA);
-  // console.log(teamB);
   let groups = generateTeams(teamA, teamB);
   let groupA = groups[0];
   let groupB = groups[1];
-  // console.log(groupA);
-  // console.log(groupB);
   let report = teamAlgorithm(groupA, groupB);
-  // console.log(report);
 
   return (
     <div className={styles.fightingComponent}>
+      <div className={styles.teamsDisplay}>
+        <TeamCard team={teamA} />
+        <TeamCard team={teamB} />
+      </div>
       {report && <TeamFightReport report={report} />}
       <div className={styles.endgameChoice}>
         <Link to="/team">
--- src/components/Team Fight Report/TeamFightReport.jsx
+++ src/components/Team Fight Report/TeamFightReport.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import styles from './FightingReport.module.css';
+import styles from './TeamFightReport.module.css';
 
 export default function TeamFightReport(props) {
   const { report } = props;
 
+  const announcement = report.shift();
+  const result = report.pop();
+
   return (
     <div className={styles.reportContainer}>
+      <div className={styles.fightingAnnouncement}>{announcement}</div>
       {report.map((x, i) => {
         return (
           <div className={styles.fightingAction} key={i}>
@@ -14,6 +18,7 @@ export default function TeamFightReport(props) {
           </div>
         );
       })}
+      <div className={styles.fightingResult}>{result}</div>
     </div>
   );
 }
