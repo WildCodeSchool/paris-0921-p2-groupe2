e75ea95145f849eb77ece09dca01e71a9564728e
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-11-17 11:59:36 +0100

    Updated and styled the team report

--- src/components/Team Fight Report/TeamFightReport.jsx
+++ src/components/Team Fight Report/TeamFightReport.jsx
@@ -5,12 +5,10 @@ import styles from './TeamFightReport.module.css';
 export default function TeamFightReport(props) {
   const { report } = props;
 
-  const announcement = report.shift();
   const result = report.pop();
 
   return (
     <div className={styles.reportContainer}>
-      <div className={styles.fightingAnnouncement}>{announcement}</div>
       {report.map((x, i) => {
         return (
           <div className={styles.fightingAction} key={i}>
