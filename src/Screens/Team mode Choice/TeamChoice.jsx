bf6a14d0b8921fa27fc1a04137fb77121436a7d8
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-11-18 15:28:56 +0100

    Uniformised the header of team fight

--- src/Screens/Team mode Choice/TeamChoice.jsx
+++ src/Screens/Team mode Choice/TeamChoice.jsx
@@ -35,7 +35,7 @@ export default function TeamChoice() {
 
   return (
     <div>
-      <h2 className={styles.teamTitle}>TEAM FIGHT</h2>
+      <h2 className={styles.challengeTitle}>Team Fight</h2>
       <div className={styles.teamsContainer}>
         {teamA && <SelectedTeam team={teamA} title={'Team A'} />}
         {teamB && <SelectedTeam team={teamB} title={'Team B'} />}
